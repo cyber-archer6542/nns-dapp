cyber-archer6542/nns-dapp

commit e23058c5918f4eb3e7a3d639c0703000a80bb979

style: fix small modal height on mobile devices (#931)
# Motivation

Fix small modal height viewport overflow on mobile devices

# Changes

- use wrapper height as max-height for small modal

frontend/svelte/src/lib/modals/Modal.svelte
@@ -147,7 +147,12 @@
       width: var(--modal-big-width);
     }
 
-    height: min(calc(100% - var(--padding-6x)), var(--modal-max-height));
+    --modal-wrapper-height: min(
+      calc(100% - var(--padding-6x)),
+      var(--modal-max-height)
+    );
+
+    height: var(--modal-wrapper-height);
     max-width: calc(100vw - var(--padding-4x));
 
     --modal-toolbar-height: 35px;
@@ -230,5 +235,6 @@
 
   .small {
     height: fit-content;
+    max-height: var(--modal-wrapper-height);
   }
 </style>

commit d8e3e7f20d7b2e0ae0105c52f79b4dece46a85c1

style: fix modal height (#939)
# Motivation

Fix broken modals introduced by [#931](https://github.com/dfinity/nns-dapp/pull/931)

# Changes

- use `100vh` instead of `100%` for Safari (😩)

frontend/svelte/src/lib/modals/Modal.svelte
@@ -147,7 +147,12 @@
       width: var(--modal-big-width);
     }
 
-    height: min(calc(100% - var(--padding-6x)), var(--modal-max-height));
+    --modal-wrapper-height: min(
+      calc(100vh - var(--padding-6x)),
+      var(--modal-max-height)
+    );
+
+    height: var(--modal-wrapper-height);
     max-width: calc(100vw - var(--padding-4x));
 
     --modal-toolbar-height: 35px;
@@ -230,5 +235,6 @@
 
   .small {
     height: fit-content;
+    max-height: var(--modal-wrapper-height);
   }
 </style>