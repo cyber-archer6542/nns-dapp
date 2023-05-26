cyber-archer6542/nns-dapp

commit b432937de17530fecc47f38384eae290037e4350

GIX-1505: Fix prod preview (#2377)
# Motivation

Fix `npm run preview` which runs a production-like environment.

# Changes

Main change:
* Logic inside `getEnvVars`. Force to use the build env vars if the port
is set. This is the case for local dev server and preview.

Minor changes:
* Add `raw` to the SNS aggregator url when running from localhost as
well. This is skipped if the `url` of the aggregator already includes
`localhost`.
* New env util `isLocalhost`.

# Tests

* Test new env util.

I tested the three ways:
* Deploy to testnet
* Local dev server
* Preview

# Docs

Add a comment on how to run `npm run preview`.

frontend/src/lib/constants/environment.constants.ts
@@ -1,22 +1,25 @@
 import { getEnvVars } from "$lib/utils/env-vars.utils";
-import { addRawToUrl } from "$lib/utils/env.utils";
+import { addRawToUrl, isLocalhost } from "$lib/utils/env.utils";
+import { isBrowser } from "@dfinity/auth-client/lib/cjs/storage";
 
 const envVars = getEnvVars();
 
 export const DFX_NETWORK = envVars.dfxNetwork;
 export const HOST = envVars.host;
 export const DEV = import.meta.env.DEV;
+
 export const FETCH_ROOT_KEY: boolean = envVars.fetchRootKey === "true";
 
 const snsAggregatorUrlEnv = envVars.snsAggregatorUrl ?? "";
 const snsAggregatorUrl = (url: string) => {
   try {
     const { hostname } = new URL(url);
-    if (hostname.includes("localhost") || hostname.includes("127.0.0.1")) {
+    if (isLocalhost(hostname)) {
       return url;
     }
 
-    if (DEV) {
+    // If the nns-dapp is running in localhost, we need to add `raw` to the URL to avoid CORS issues.
+    if (isBrowser && isLocalhost(window.location.hostname)) {
       return addRawToUrl(url);
     }
 

frontend/src/lib/utils/env-vars.utils.ts
@@ -1,7 +1,7 @@
 import { isBrowser } from "@dfinity/auth-client/lib/cjs/storage";
 import { isNullish } from "@dfinity/utils";
 
-const localDev = import.meta.env.DEV;
+const localDevelopment = import.meta.env.DEV;
 
 type EnvironmentVars = {
   // Environments without ckBTC canisters are valid
@@ -127,9 +127,29 @@ const getBuildEnvVars = (): EnvironmentVars => {
   return envVars;
 };
 
+/**
+ * Returns the environment variables depending on the environment.
+ *
+ * The environment variables might be set in the HTML or from the build environment variables.
+ *
+ * We use the build environment variables when one of the following conditions is true:
+ * - Not in the browser
+ * - Local development server
+ *
+ * All the other scenarios use the HTML environment variables.
+ *
+ * @returns {EnvironmentVars}
+ */
 export const getEnvVars = (): EnvironmentVars => {
-  if (isBrowser && !localDev) {
-    return getHtmlEnvVars();
+  // There are two instances of local dev server:
+  // - The local dev server started by `npm run dev`
+  // - The local dev server started by `npm run preview`. The only way to distinguis this one is by the port.
+  // We need to check `isBrowser` to skip the check when running the build.
+  const isDevServer =
+    localDevelopment ||
+    (isBrowser && ["5173", "4173"].includes(window.location.port));
+  if (!isBrowser || isDevServer) {
+    return getBuildEnvVars();
   }
-  return getBuildEnvVars();
+  return getHtmlEnvVars();
 };

frontend/src/lib/utils/env.utils.ts
@@ -28,3 +28,6 @@ export const addRawToUrl = (urlString: string): string => {
 
   return hasTrailingSlash ? url.toString() : url.toString().slice(0, -1);
 };
+
+export const isLocalhost = (hostname: string) =>
+  hostname.includes("localhost") || hostname.includes("127.0.0.1");

frontend/src/tests/lib/utils/env.utils.spec.ts
@@ -2,7 +2,11 @@
  * @jest-environment jsdom
  */
 
-import { addRawToUrl, isNnsAlternativeOrigin } from "$lib/utils/env.utils";
+import {
+  addRawToUrl,
+  isLocalhost,
+  isNnsAlternativeOrigin,
+} from "$lib/utils/env.utils";
 
 describe("env-utils", () => {
   describe("isNnsAlternativeOrigin", () => {
@@ -121,6 +125,24 @@ describe("env-utils", () => {
       expect(() => addRawToUrl(invalid2)).toThrow(
         new TypeError(`Invalid URL: ${invalid2}`)
       );
+    });
+  });
+
+  describe("isLocalhost", () => {
+    it("return false when hostname is not localhost", () => {
+      expect(
+        isLocalhost(
+          "53zcu-tiaaa-aaaaa-qaaba-cai.medium09.testnet.dfinity.network"
+        )
+      ).toBe(false);
+      expect(isLocalhost("internetcomputer.org")).toBe(false);
+      expect(isLocalhost("nns.ic0.app")).toBe(false);
+    });
+
+    it("return true when hostname is localhost", () => {
+      expect(isLocalhost("localhost:3000")).toBe(true);
+      expect(isLocalhost("127.0.0.1:3000")).toBe(true);
+      expect(isLocalhost("xxxx.localhost")).toBe(true);
     });
   });
 });

commit 1ee11e300d7d9feb1e2c5ade277d91be4ce3cc11

Fix reproducibility by not explicitly importing cjs modules from agent-js in esm (#2584)
# Motivation

@dskloetd figured out that our frontend reproducibility issue started
with PR #2377. After having a look at the PR we noticed it introduced an
explicit import of a CJS module of agent-js within the code that is
bundled with ESM. As I suspected that our issue was linked to pollyfying
the Node modules for the browser, we replaced the import by inline code
and as a result, the reproducibility was improved.

Using the `./script/test-reproducibility` script, on my machine, I am
now able to run 100 successful check in a row.

# Changes

- remove explicit CJS `import`
- use SvelteKit `browser` information when possible
- when not (see PR for explanation) use a custom `isBrowser` information

frontend/src/lib/components/ic/GetTokens.svelte
@@ -17,7 +17,7 @@
   import { ICPToken, type Token } from "@dfinity/nns";
   import { snsTokenSymbolSelectedStore } from "$lib/derived/sns/sns-token-symbol-selected.store";
   import { authSignedInStore } from "$lib/derived/auth.derived";
-  import { isBrowser } from "@dfinity/auth-client/lib/cjs/storage";
+  import { browser } from "$app/environment";
 
   let visible = false;
   let transferring = false;
@@ -77,7 +77,7 @@
   $: selectedProjectId,
     (async () => {
       // This was executed at build time and it depends on `window` in `base64ToUInt8Array` helper inside dev.api.ts
-      if (isBrowser) {
+      if (browser) {
         tokenBalanceE8s = await getTestBalance(selectedProjectId);
       }
     })();

frontend/src/lib/constants/environment.constants.ts
@@ -1,6 +1,5 @@
 import { getEnvVars } from "$lib/utils/env-vars.utils";
-import { addRawToUrl, isLocalhost } from "$lib/utils/env.utils";
-import { isBrowser } from "@dfinity/auth-client/lib/cjs/storage";
+import { addRawToUrl, isBrowser, isLocalhost } from "$lib/utils/env.utils";
 
 const envVars = getEnvVars();
 

frontend/src/lib/utils/env-vars.utils.ts
@@ -1,4 +1,4 @@
-import { isBrowser } from "@dfinity/auth-client/lib/cjs/storage";
+import { isBrowser } from "$lib/utils/env.utils";
 import { isNullish } from "@dfinity/utils";
 
 const localDevelopment = import.meta.env.DEV;

frontend/src/lib/utils/env.utils.ts
@@ -1,5 +1,17 @@
 import { NNS_IC_ORG_ALTERNATIVE_ORIGIN } from "$lib/constants/origin.constants";
 
+/**
+ * To be use when Rollup fails to resolve import "$app/environment". This can happen because we manually define chunks and SvelteKit is not entirely resilient to bundler reordering.
+ * Example of SvelteKit issue: https://github.com/sveltejs/kit/pull/9808
+ *
+ * Stacktrace of the `npm run build` error that can lead to use `isBrowser` instead of `browser`:
+ *
+ * [vite:worker] [vite]: Rollup failed to resolve import "__sveltekit/environment" from "/.../nns-dapp/frontend/node_modules/@sveltejs/kit/src/runtime/app/environment.js".
+ * This is most likely unintended because it can break your application at runtime.
+ * If you do want to externalize this module explicitly add it to `build.rollupOptions.external`
+ */
+export const isBrowser = typeof window !== "undefined";
+
 export const isNnsAlternativeOrigin = (): boolean => {
   const {
     location: { origin },